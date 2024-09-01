28d6609f3cf592a39cd6fc261ab35ed40b49db8b
Tobias Folie <tobias@folie.tirol>
2024-09-01 15:12:26 +0200

    fetch activities from DailyActivities Scene instead of via group

--- src/activities/tracking_view.gd
+++ src/activities/tracking_view.gd
@@ -10,14 +10,14 @@ func _on_fold_button_pressed() -> void:
 	to_compact_view.emit()
 
 func _on_day_selector_new_day(_old_day: Date, new_day: Date) -> void:
-	for tracker: Activity in get_tree().get_nodes_in_group("activity_tracking"):
+	for tracker: Activity in daily_activities.get_activity_nodes():
 		if tracker.is_active():
 			currently_active_hidden.emit(tracker)
 			
 	daily_activities.open_new_day(new_day)
 
 func init_active(activty_name: String, time: int) -> void:
-	for tracker: Activity in get_tree().get_nodes_in_group("activity_tracking"):
+	for tracker: Activity in daily_activities.get_activity_nodes():
 		if tracker.get_activity_name() == activty_name:
 			tracker.time_seconds = time
 			tracker.activate()
